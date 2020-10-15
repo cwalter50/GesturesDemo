82c58d4cfabebf94279102550b5f808afe4fe7bc
cwalter50 <walt50@gmail.com>
2020-10-15 10:30:15 -0400

    MVP

--- GesturesDemo/ViewController.swift
+++ GesturesDemo/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func imageviewTapped(_ sender: UITapGestureRecognizer)
+    {
+        print("Black Panther tapped")
+    }
+    
+    @IBAction func unicornPanned(_ sender: UIPanGestureRecognizer)
+    {
+        // 1
+         let translation = sender.translation(in: view)
 
+         // 2
+         guard let gestureView = sender.view else {
+           return
+         }
+
+         gestureView.center = CGPoint(
+           x: gestureView.center.x + translation.x,
+           y: gestureView.center.y + translation.y
+         )
+
+         // 3
+         sender.setTranslation(.zero, in: view)
+    }
 }
 
